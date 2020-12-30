c9931d2287b3269e9b27e4adb06e65c8e61c5caa
Petr Šnobl <snoblucha@email.cz>
2020-12-30 17:38:01 +0100

    enabled back document serializing

--- app/serializers/attachment_serializer.rb
+++ app/serializers/attachment_serializer.rb
@@ -56,8 +56,6 @@ class AttachmentSerializer < ActiveModel::Serializer
   private
 
   def supported?
-    #TODO: enable back
-    return false
     object.filesize > 0 &&
       object.filesize < MAX_SIZE &&
       (extension_supported? || content_type_supported?) &&
